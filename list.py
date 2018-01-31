2728963d054d4701096c4e14348a5c4b9f8454b0
Henry Spivey <spivotron@gmail.com>
2018-01-31 14:33:10 -0800

    adding in list

---
+++ list.py
@@ -0,0 +1,26 @@
+# Supported methods:
+# List()
+# 	- Creates a new list that is empty
+# add(item)
+# 	- adds a new item to the list.  Returns nothing
+# remove(item)
+# 	- removes the item from the list. Returns nothing
+# search(item)
+# 	- searches for the item and returns a boolean
+# isEmpty()
+# 	- tests whether the list is empty or not.  Returns a boolean
+# size()
+# 	- return the number of items in the list. Returns an integer value
+# append(item)
+# 	- adds a new item to the end of the list. Assume that the item is not already in the list
+# index(item)
+# 	- returns the position of the item in the list.  Returns the index
+# insert(pos, item)
+# 	- adds item to the list as pos. Assume that the item is not in the list and there are enough existing items to have position pos
+# pop()
+# 	- removes and returns the last item in the list.  Assume the list has at least 1 item
+# pop(pos)
+# 	- removes and returns the item in the list at pos.  Assume the item is in the list 
+
+# Basic building block is the Node
+# Node holds data and pointer to next
